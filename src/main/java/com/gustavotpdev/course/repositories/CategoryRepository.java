ad8ba807c413b5fff2cb13fda88267c55942ac27
Gustavotp443 <gustavotp443@gmail.com>
2023-02-23 11:41:13 -0300

    Category

--- src/main/java/com/gustavotpdev/course/repositories/CategoryRepository.java
+++ src/main/java/com/gustavotpdev/course/repositories/CategoryRepository.java
@@ -3,9 +3,10 @@ package com.gustavotpdev.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gustavotpdev.course.entities.Order;
+import com.gustavotpdev.course.entities.Category;
 
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Long>{
+public interface CategoryRepository extends JpaRepository<Category,Long>{
+	
 }
