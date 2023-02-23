b1de853faffec9f8064728e93786888106945bc9
Gustavotp443 <gustavotp443@gmail.com>
2023-02-23 16:20:53 -0300

    Subtotal and TotalMethods

--- src/main/java/com/gustavotpdev/course/entities/OrderItem.java
+++ src/main/java/com/gustavotpdev/course/entities/OrderItem.java
@@ -74,7 +74,11 @@ public class OrderItem implements Serializable{
 	}
 
 
-
+	public Double getSubTotal() {
+		return price*quantity;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
