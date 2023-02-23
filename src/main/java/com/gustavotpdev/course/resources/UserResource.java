e1f6b230da1093374fe3df39902aa5e9e67254f1
Gustavotp443 <gustavotp443@gmail.com>
2023-02-23 16:39:15 -0300

    User insert

--- src/main/java/com/gustavotpdev/course/resources/UserResource.java
+++ src/main/java/com/gustavotpdev/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.gustavotpdev.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavotpdev.course.entities.User;
 import com.gustavotpdev.course.services.UserService;
@@ -25,9 +29,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value="/{id}")
+	@GetMapping(value="/{id}")			//Path da Url ID
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	
+	@PostMapping					//Request o que vira no Body 		
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj= service.insert(obj);
+		//DEVOLVER UM 201
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }
--- src/main/java/com/gustavotpdev/course/services/UserService.java
+++ src/main/java/com/gustavotpdev/course/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();	//Se o valor existir ele retorna pelo .get()
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
